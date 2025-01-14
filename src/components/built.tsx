bbcd083675534dc5756594e31240f93169b6c582
degiam <degiam@ymail.com>
2025-01-14 16:16:21 +0700

    update: built

--- src/components/built.tsx
+++ src/components/built.tsx
@@ -1,40 +1,46 @@
 const Built = () => {
   return (
-    <div class="text-slate-400/60 dark:text-slate-600 text-xs flex items-center justify-center gap-1 mt-14">
-      <span>Built on</span>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
-        <path fill="#76b3e1" d="M22.728,6.299c-0,-0 -7.112,-5.37 -12.631,-4.116l-0.484,0.134c-0.755,0.217 -1.415,0.681 -1.876,1.317c-0.074,0.108 -0.142,0.22 -0.202,0.336l-2.039,3.463l3.521,0.687c1.456,1.027 3.283,1.386 5.02,0.987l6.271,1.225l2.42,-4.033Z" />
-        <path fill="url(#solid_logo_linear_1)" d="M22.728,6.299c-0,-0 -7.112,-5.37 -12.631,-4.116l-0.484,0.134c-0.755,0.217 -1.415,0.681 -1.876,1.317c-0.074,0.108 -0.142,0.22 -0.202,0.336l-2.039,3.463l3.521,0.687c1.456,1.027 3.283,1.386 5.02,0.987l6.271,1.225l2.42,-4.033Z" />
-        <path fill="#518ac8" d="M7.674,6.231l-0.487,0.136c-2.252,0.727 -3.022,2.834 -1.728,4.681c1.526,1.918 4.076,2.721 6.426,2.024l8.414,-2.74c0,0 -7.097,-5.356 -12.625,-4.101Z" />
-        <path fill="url(#solid_logo_linear_2)" d="M7.674,6.231l-0.487,0.136c-2.252,0.727 -3.022,2.834 -1.728,4.681c1.526,1.918 4.076,2.721 6.426,2.024l8.414,-2.74c0,0 -7.097,-5.356 -12.625,-4.101Z" />
-        <path fill="url(#solid_logo_linear_3)" d="M18.76,12.305c-1.523,-1.928 -4.081,-2.738 -6.436,-2.038l-8.407,2.725l-2.645,4.709l15.059,2.566l2.7,-4.804c0.539,-0.92 0.484,-2.08 -0.271,-3.158Z" />
-        <path fill="url(#solid_logo_linear_4)" d="M16.115,17.014c-1.523,-1.92 -4.071,-2.729 -6.423,-2.038l-8.42,2.725c0,0 7.126,5.37 12.631,4.116l0.484,-0.134c2.255,-0.715 3.025,-2.822 1.728,-4.669Z" />
-        <defs>
-          <linearGradient id="solid_logo_linear_1" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(16.8011,8.16394,-8.16394,16.8011,4.44465,1.93475)">
-            <stop stop-color="#76b3e1" stop-opacity="1" offset="0" />
-            <stop stop-color="#76b3e1" stop-opacity="1" offset="0.1" />
-            <stop stop-color="#a5cde8" stop-opacity="1" offset="0.3" />
-            <stop stop-color="#76b3e1" stop-opacity="1" offset="1" />
-          </linearGradient>
-          <linearGradient id="solid_logo_linear_2" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(-2.94176,9.7973,-9.7973,-2.94176,13.6604,5.93406)">
-            <stop stop-color="#76b3e1" stop-opacity="1" offset="0" />
-            <stop stop-color="#4377bb" stop-opacity="1" offset="0.5" />
-            <stop stop-color="#1f3b77" stop-opacity="1" offset="1" />
-          </linearGradient>
-          <linearGradient id="solid_logo_linear_3" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(16.9892,11.5504,-11.5504,16.9892,3.21037,10.207)">
-            <stop stop-color="#315aa9" stop-opacity="1" offset="0" />
-            <stop stop-color="#518ac8" stop-opacity="1" offset="0.5" />
-            <stop stop-color="#315aa9" stop-opacity="1" offset="1" />
-          </linearGradient>
-          <linearGradient id="solid_logo_linear_4" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(-6.85554,25.1389,-25.1389,-6.85554,10.8812,11.5895)">
-            <stop stop-color="#4377bb" stop-opacity="1" offset="0" />
-            <stop stop-color="#1a336b" stop-opacity="1" offset="0.5" />
-            <stop stop-color="#1a336b" stop-opacity="1" offset="1" />
-          </linearGradient>
-        </defs>
-      </svg>
-      <span>Solid</span>
-    </div>
+    <section class="text-xs flex items-center justify-center flex-col gap-2 mt-14">
+      <div class="text-slate-400/60 dark:text-slate-600 text-xs flex items-center justify-center gap-1">
+        <span>Built on</span>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
+          <path fill="#76b3e1" d="M22.728,6.299c-0,-0 -7.112,-5.37 -12.631,-4.116l-0.484,0.134c-0.755,0.217 -1.415,0.681 -1.876,1.317c-0.074,0.108 -0.142,0.22 -0.202,0.336l-2.039,3.463l3.521,0.687c1.456,1.027 3.283,1.386 5.02,0.987l6.271,1.225l2.42,-4.033Z" />
+          <path fill="url(#solid_logo_linear_1)" d="M22.728,6.299c-0,-0 -7.112,-5.37 -12.631,-4.116l-0.484,0.134c-0.755,0.217 -1.415,0.681 -1.876,1.317c-0.074,0.108 -0.142,0.22 -0.202,0.336l-2.039,3.463l3.521,0.687c1.456,1.027 3.283,1.386 5.02,0.987l6.271,1.225l2.42,-4.033Z" />
+          <path fill="#518ac8" d="M7.674,6.231l-0.487,0.136c-2.252,0.727 -3.022,2.834 -1.728,4.681c1.526,1.918 4.076,2.721 6.426,2.024l8.414,-2.74c0,0 -7.097,-5.356 -12.625,-4.101Z" />
+          <path fill="url(#solid_logo_linear_2)" d="M7.674,6.231l-0.487,0.136c-2.252,0.727 -3.022,2.834 -1.728,4.681c1.526,1.918 4.076,2.721 6.426,2.024l8.414,-2.74c0,0 -7.097,-5.356 -12.625,-4.101Z" />
+          <path fill="url(#solid_logo_linear_3)" d="M18.76,12.305c-1.523,-1.928 -4.081,-2.738 -6.436,-2.038l-8.407,2.725l-2.645,4.709l15.059,2.566l2.7,-4.804c0.539,-0.92 0.484,-2.08 -0.271,-3.158Z" />
+          <path fill="url(#solid_logo_linear_4)" d="M16.115,17.014c-1.523,-1.92 -4.071,-2.729 -6.423,-2.038l-8.42,2.725c0,0 7.126,5.37 12.631,4.116l0.484,-0.134c2.255,-0.715 3.025,-2.822 1.728,-4.669Z" />
+          <defs>
+            <linearGradient id="solid_logo_linear_1" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(16.8011,8.16394,-8.16394,16.8011,4.44465,1.93475)">
+              <stop stop-color="#76b3e1" stop-opacity="1" offset="0" />
+              <stop stop-color="#76b3e1" stop-opacity="1" offset="0.1" />
+              <stop stop-color="#a5cde8" stop-opacity="1" offset="0.3" />
+              <stop stop-color="#76b3e1" stop-opacity="1" offset="1" />
+            </linearGradient>
+            <linearGradient id="solid_logo_linear_2" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(-2.94176,9.7973,-9.7973,-2.94176,13.6604,5.93406)">
+              <stop stop-color="#76b3e1" stop-opacity="1" offset="0" />
+              <stop stop-color="#4377bb" stop-opacity="1" offset="0.5" />
+              <stop stop-color="#1f3b77" stop-opacity="1" offset="1" />
+            </linearGradient>
+            <linearGradient id="solid_logo_linear_3" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(16.9892,11.5504,-11.5504,16.9892,3.21037,10.207)">
+              <stop stop-color="#315aa9" stop-opacity="1" offset="0" />
+              <stop stop-color="#518ac8" stop-opacity="1" offset="0.5" />
+              <stop stop-color="#315aa9" stop-opacity="1" offset="1" />
+            </linearGradient>
+            <linearGradient id="solid_logo_linear_4" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(-6.85554,25.1389,-25.1389,-6.85554,10.8812,11.5895)">
+              <stop stop-color="#4377bb" stop-opacity="1" offset="0" />
+              <stop stop-color="#1a336b" stop-opacity="1" offset="0.5" />
+              <stop stop-color="#1a336b" stop-opacity="1" offset="1" />
+            </linearGradient>
+          </defs>
+        </svg>
+        <span>Solid</span>
+      </div>
+
+      <div class="text-slate-400/60 dark:text-slate-600 hover:text-black dark:hover:text-white transition underline hover:no-underline">
+        <a href="https://github.com/degiam/degiam-zip" target="_blank" rel="noopener noreferrer">Fork at GitHub</a>
+      </div>
+    </section>
   )
 }
 
