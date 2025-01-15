1b790f31c2ce263948755a0ff5d0c1c98e468d36
degiam <degiam@ymail.com>
2025-01-15 16:04:35 +0700

    update: delete button

--- src/components/compress.tsx
+++ src/components/compress.tsx
@@ -74,6 +74,10 @@ function Compress(props: CompressProps) {
     setErrorFile((prev) => [...prev, newError])
   }
 
+  const handleRemoveFile = (fileToRemove: File) => {
+    props.setUploadedFiles(props.uploadedFiles.filter((file) => file.name !== fileToRemove.name))
+  }
+
   createEffect(() => {
     const interval = setInterval(() => {
       const currentTime = Date.now()
@@ -195,6 +199,31 @@ function Compress(props: CompressProps) {
                           <path d="M12 4l0 12" />
                         </svg>
                         <span class="sr-only">Unduh</span>
+                      </button>
+                      <button
+                        type="button"
+                        onClick={() => handleRemoveFile(file)}
+                        class="ml-3 text-sm text-red-500 hover:text-red-700"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        >
+                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                          <path d="M4 7l16 0" />
+                          <path d="M10 11l0 6" />
+                          <path d="M14 11l0 6" />
+                          <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                          <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                        </svg>
+                        <span class="sr-only">Hapus</span>
                       </button>
                     </div>
                   </li>
